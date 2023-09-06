c1ab2a483587f518d7198dcba9996c33c3dda956
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-09-06 14:51:14 +0200

    Update robot.py

--- robot.py
+++ robot.py
@@ -252,7 +252,6 @@ if sys.argv[1] == "test":
 elif sys.argv[1] == "eight":    
     for i in range(0, int(sys.argv[2])):
         smoothTurn(r, "right")  
-        straight64(r, 0.8)
         smoothTurn(r, "left")  
 
 elif sys.argv[1] == "circle":
