8ffff88ffff6eea843f3d62ff2597130516c9097
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-09-06 14:34:10 +0200

    Update robot.py

--- robot.py
+++ robot.py
@@ -227,10 +227,10 @@ def sv(int):
 
 def smoothTurn(r, direction):
     if (direction == "left"):
-        r.go_diff(84, 38, 1, 1)
+        r.go_diff(38, 84, 1, 1)
         sleep(8.6)
     else:
-        r.go_diff(40, 80, 1, 1)
+        r.go_diff(80, 40, 1, 1)
         sleep(8.2)
 
 r = Robot()
