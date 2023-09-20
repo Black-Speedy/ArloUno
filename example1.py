44b8c8c797251b225483d7c2b4b888b66c55fd88
Mikkelbos <mikkeldollerup@gmail.com>
2023-09-20 13:12:27 +0200

    Update example1.py

--- example1.py
+++ example1.py
@@ -67,7 +67,7 @@ while cv2.waitKey(4) == -1:  # Wait for a key pressed event
         radians = tvecs[0][0][0]
         degrees = np.degrees(radians)+ angle_error
         # Print tvecs in format: distance, height, angle in degrees
-        print(f"distance = {tvecs[0][0][2]} degrees = {degrees}°")
-        #print({rvecs[0][0][2]}, {rvecs[0][0][1]}, {rvecs[0][0][0]})
+        #print(f"distance = {tvecs[0][0][2]} degrees = {degrees}°")
+        print({rvecs[0][0][2]}, {rvecs[0][0][1]}, {rvecs[0][0][0]})
 
 
