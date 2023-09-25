53f4a5cd2e1c7304c73cd8b2657729129801cfbb
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-09-25 14:40:38 +0200

    Update robot.py

--- robot.py
+++ robot.py
@@ -314,9 +314,8 @@ class RobotController():
                 print(result)
                 self.box = result
                 self.r.stop()
-                self.ds = DriveState.EXIT
-                """ self.ds = DriveState.STRAIGHT
-                self.straight64(int(result[0]/100) - 40) """
+                self.ds = DriveState.STRAIGHT
+                self.straight64(int(result[0]*100) - 40)
                 return result
             else:
                 print("found wrong box?")
