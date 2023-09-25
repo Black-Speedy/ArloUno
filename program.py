6a5bb8bec1974d5966335d271ee741e71c50e0bd
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-09-25 14:30:20 +0200

    Update program.py

--- program.py
+++ program.py
@@ -2,5 +2,10 @@ from robot import *
 r = RobotController()
 
 maxTime = 60 + time.perf_counter()
+
+ctime = time.perf_counter()
 while(maxTime > time.perf_counter()):
-    r.update()
+    # only update every 0.1 seconds
+    if ctime + 0.1 < time.perf_counter():
+        r.update()
+        ctime = time.perf_counter()
