e5e7cb410ce7dda44ee7362f260c5ec3f6d8379d
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-09-27 09:41:53 +0200

    Update map_program.py

--- map_program.py
+++ map_program.py
@@ -19,8 +19,8 @@ for tvec in results:
     else:
         x = int(640 + tvec[0][2]*100 * np.cos(radians + np.deg2rad(11)))
         
-    y = 720 - int(tvec[0][2]*100)
-    print(f"x value = {x} y value = {y}")
+    y = int(720 - int(tvec[0][2]*100)/2)
+    print(f"x value = {x} y value = {y-640}")
     img = cv2.circle(img, (x, y), 50, (255, 255, 255), 3)
 
 cv2.imwrite("tests.png", img)
