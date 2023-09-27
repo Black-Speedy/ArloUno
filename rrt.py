9ad4b6b63a38a28faaec4da57828683e1dd606d1
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-09-27 14:00:19 +0200

    Update rrt.py

--- rrt.py
+++ rrt.py
@@ -178,17 +178,23 @@ class RRT:
     def check_collision_free(self, node):
         if node is None:
             return False
-        for p in node.path:
-            #check if the node will collide, considering all nodes in a radius around the robot
-            for i in range(-1, 2):
-                for j in range(-1, 2):
-                    if self.map.in_collision(np.array(p([p.pos[0] + i, p.pos[1] + j]))):
-                        return False
-                    
-            if self.map.in_collision():
-                return False
 
-            return True
+        # Define the robot's width (assuming it's symmetric)
+        robot_width = 0.35
+
+        for p in node.path:
+            # Iterate over a grid of points within the robot's footprint
+            for x_offset in range(-robot_width // 2, (robot_width // 2) + 1):
+                for y_offset in range(-robot_width // 2, (robot_width // 2) + 1):
+                    # Calculate the adjusted position
+                    adjusted_position = np.array(p) + np.array([x_offset, y_offset])
+
+                    # Check if the adjusted position is in collision
+                    if self.map.in_collision(adjusted_position):
+                        return False
+
+        return True
+
 
 
 import grid_occ, robot_models
