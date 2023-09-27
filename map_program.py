b3fd317a45df5f170062a0688a0bcd874e837803
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-09-27 09:27:46 +0200

    Update map_program.py

--- map_program.py
+++ map_program.py
@@ -7,11 +7,14 @@ img = np.zeros((720, 1280, 3), np.uint8)
 
 results = lookBox(-1)
 
+angle_error = 11
 
 print(results)
 
 for tvec in results:
-    img = cv2.circle(img, (640, 720 - int(tvec[0][2]*100)), 50, (255, 255, 255), 3)
+    radians = tvec[0][0]
+    degrees = np.degrees(radians)+ angle_error
+    img = cv2.circle(img, (640 + degrees, 720 - int(tvec[0][2]*100)), 50, (255, 255, 255), 3)
 
 cv2.imwrite("tests.png", img)
 
