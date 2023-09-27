17bb6ea20dfc05242d25e4f00eb5327088ceca89
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-09-27 15:13:36 +0200

    d

---
+++ main.py
@@ -0,0 +1,57 @@
+from robot_driving_states import RobotController
+from robot import Robot
+from rrt import *
+
+def main():
+
+    path_res = 0.05
+    map = grid_occ.GridOccupancyMap(low=(-1, 0), high=(1, 2), res=path_res)
+    map.populate()
+
+    robot = robot_models.PointMassModel(ctrl_range=[-path_res, path_res])   #
+
+    rrt = RRT(
+        start=[0, 0],
+        goal=[0, 1.9],
+        robot_model=robot,
+        map=map,
+        expand_dis=0.2,
+        path_resolution=path_res,
+    )
+
+    show_animation = False
+    metadata = dict(title="RRT Test")
+    writer = FFMpegWriter(fps=15, metadata=metadata)
+    fig = plt.figure()
+    path = rrt.planning(animation=show_animation, writer=writer)
+
+    with writer.saving(fig, "rrt_test.mp4", 100):
+
+        if path is None:
+            print("Cannot find path")
+        else:
+            print("found path!!")
+
+            # Draw final path
+            if show_animation:
+                rrt.draw_graph()
+                plt.plot([x for (x, y) in path], [y for (x, y) in path], '-r')
+                plt.grid(True)
+                plt.pause(0.01)  # Need for Mac
+                plt.show()
+                writer.grab_frame()
+
+    rrt.draw_graph()
+    plt.plot([x for (x, y) in path], [y for (x, y) in path], '-r')
+    plt.grid(True)
+    plt.pause(0.01)  # Need for Mac
+    plt.savefig("map_with_path.png")
+
+    r = RobotController(path)
+
+    while(True):
+        r.update()
+
+
+if __name__ == '__main__':
+    main()
--- robot_driving_states.py
+++ robot_driving_states.py
@@ -1,7 +1,82 @@
 from enum import Enum
+from robot import *
+
 class DriveState(Enum):
     STOP = 0
     STRAIGHT = 1
     TURN = 2
     SEARCH = 3
     EXIT = 4
+
+class RobotController():
+    def __init__(self):
+        self.r = Robot()
+        self.ds = DriveState.SEARCH
+        self.stopTimer = 0
+        self.stopTurnTimer = 0
+        self.path = []
+        self.theta = 0
+        self.currentPoint = 0
+    
+    def straight64(self, cm):
+        self.r.go_diff(65, 70, 1, 1)
+        self.stopTimer = time.perf_counter() + cm * 2.24 / 100
+
+    def turnDegree(self, degrees, direction, convert=False):
+        print("turning")
+        if convert:
+            radians = degrees * np.pi / 180
+        else:
+            radians = degrees
+        if radians < 0:
+            radians = radians + 2 * np.pi
+        if direction == "left":
+            self.r.go_diff(30, 30, 0, 1)
+            self.theta += radians
+        else:
+            self.r.go_diff(30, 30, 1, 0)
+            self.theta -= radians
+        self.stopTurnTimer = time.perf_counter() + (degrees / 90) * 1.95
+
+    def update(self):
+        if (self.ds == DriveState.STOP):
+            if (self.stopTimer < time.perf_counter()):
+                self.ds = DriveState.SEARCH
+
+        elif (self.ds == DriveState.TURN):
+            if (self.stopTurnTimer < time.perf_counter()):
+                self.ds = DriveState.STOP
+                self.stopTimer = time.perf_counter() + 0.3
+                self.r.stop()
+
+        elif (self.ds == DriveState.STRAIGHT):
+            if(self.stopTimer < time.perf_counter()):
+                self.r.stop()
+                self.ds = DriveState.STOP
+                self.stopTimer = time.perf_counter() + 0.3
+
+        elif (self.ds == DriveState.SEARCH):
+            if self.currentPoint == len(self.path) - 1:
+                self.ds = DriveState.EXIT
+                return
+            
+            # find angle to next point
+            theta = np.arctan2(self.path[self.currentPoint + 1][1] - self.path[self.currentPoint][1],
+                                 self.path[self.currentPoint][0] - self.path[self.currentPoint][0]) - self.theta
+
+            if (theta != 0):
+                # we need to turn
+                self.ds = DriveState.TURN
+                if (theta > 0):
+                    self.turnDegree(np.degrees(theta), "left")
+                else:
+                    self.turnDegree(np.degrees(theta), "right")
+            else:
+                # we need to drive straight
+                self.ds = DriveState.STRAIGHT
+                self.straight64(np.linalg.norm(self.path[self.currentPoint + 1] - self.path[self.currentPoint]))
+                self.currentPoint += 1
+
+        elif (self.ds == DriveState.EXIT):
+            print("EXIT!")
+            exit()
