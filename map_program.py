d63be9dd0706375b8fc2c246b1b0a2bfc8036c62
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-09-27 09:34:05 +0200

    Update map_program.py

--- map_program.py
+++ map_program.py
@@ -14,7 +14,8 @@ print(results)
 for tvec in results:
     radians = tvec[0][0]
     degrees = np.degrees(radians)+ angle_error
-    img = cv2.circle(img, (int(640 + degrees), 720 - int(tvec[0][2]*100)), 50, (255, 255, 255), 3)
+    img = cv2.circle(img, (int(
+        640 + tvec[0][2]*100  * np.cos(radians + np.deg2rad(11))), 720 - int(tvec[0][2]*100)), 50, (255, 255, 255), 3)
 
 cv2.imwrite("tests.png", img)
 
