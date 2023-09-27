468d94377d8e52e0c09a2c5ca656c400a06cdc1f
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-09-27 10:15:04 +0200

    Update map_program.py

--- map_program.py
+++ map_program.py
@@ -5,27 +5,39 @@ from landMark import *
 img = np.zeros((720, 720, 3), np.uint8)
 
 
-results, ids = lookBox(-1)
 
 angle_error = 11
 
 print(results)
 
-for i in range(0, len(results)):
-    radians = results[i][0][0]
-    degrees = np.degrees(radians)+ angle_error
-    if(degrees < 0):
-        x = int(360 - results[i][0][2]*100 * np.cos(radians + np.deg2rad(11)))
-    else:
-        x = int(360 + results[i][0][2]*100 * np.cos(radians + np.deg2rad(11)))
-        
-    y = int((720)/2) - int(results[i][0][2]*100)
-    print(f"x value = {x} y value = {y-640}")
-    img = cv2.circle(img, (x, y), 35, (255, 255, 255), 3)
-    # write the id of the box
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img, str(ids[i]), (x, y),
-                font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
 
 cv2.imwrite("tests.png", img)
 
+# open a window
+WIN_RF = "Example 1"
+cv2.namedWindow(WIN_RF)
+cv2.moveWindow(WIN_RF, 100, 100)
+
+
+while(True):
+    
+    results, ids = lookBox(-1)
+
+    for i in range(0, len(results)):
+        radians = results[i][0][0]
+        degrees = np.degrees(radians) + angle_error
+        if (degrees < 0):
+            x = int(360 - results[i][0][2]*100 * np.cos(radians + np.deg2rad(11)))
+        else:
+            x = int(360 + results[i][0][2]*100 * np.cos(radians + np.deg2rad(11)))
+
+        y = int((720)/2) - int(results[i][0][2]*100)
+        print(f"x value = {x} y value = {y}")
+        img = cv2.circle(img, (x, y), 35, (255, 255, 255), 3)
+        # write the id of the box
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, str(ids[i]), (x, y),
+                    font, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        
+    cv2.imshow(WIN_RF, img)
