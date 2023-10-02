800238e840712011a39cd33de8608807d10da6ac
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-10-02 15:46:47 +0200

    Update grid_occ.py

--- grid_occ.py
+++ grid_occ.py
@@ -61,7 +61,7 @@ class GridOccupancyMap(object):
             y = (results[i][0][2] + 0.175 )
             print(f"x: {x} y: {y}")
             origins.append([x, y])
-            radius.append(0.175 + 0.22+0.05)
+            radius.append(0.175 + 0.22+0.07)
 
 
         # fill the grids by checking if the grid centroid is in any of the circle
