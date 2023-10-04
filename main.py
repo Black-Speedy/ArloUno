e2da97549e5d99c795e00975ff4f94a5ae0bb40e
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-10-04 12:38:36 +0200

    s

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 def main():
 
     path_res = 0.05
-    map = grid_occ.GridOccupancyMap(low=(-1, 0), high=(1, 2), res=path_res)
+    map = grid_occ.GridOccupancyMap(low=(-3, 0), high=(3, 6), res=path_res)
     map.populate()
 
     robot = robot_models.PointMassModel(ctrl_range=[-path_res, path_res])   #
