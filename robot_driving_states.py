8f5e10108eb6011cdd924373728879a1c4149518
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-10-04 12:31:01 +0200

    Update robot_driving_states.py

--- robot_driving_states.py
+++ robot_driving_states.py
@@ -32,7 +32,7 @@ class RobotController():
         if direction == "left":
             self.r.go_diff(30, 30, 0, 1)
             self.theta += degrees
-            self.stopTurnTimer = time.perf_counter() + (degrees / 90) * 2.2 #1.95
+            self.stopTurnTimer = time.perf_counter() + (degrees / 90) * 2.4 #1.95
         else:
             self.r.go_diff(30, 30, 1, 0)
             self.theta -= degrees
