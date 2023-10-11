6043940a362ffaa1265ca9d838676e66587511a8
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-10-11 12:04:35 +0200

    Update selflocalize.py

--- Self-localization/handout/python/selflocalize.py
+++ Self-localization/handout/python/selflocalize.py
@@ -216,9 +216,9 @@ try:
                 for obj in detected_objects:
                     # check if distance is close to the recorded distance
                     if obj[0] == 1:
-                        tmp += abs(obj[1] - d_to_1)
+                        tmp += (abs(obj[1] - d_to_1) / 100)
                     elif obj[0] == 8:
-                        tmp += abs(obj[1] - d_to_8)
+                        tmp += (abs(obj[1] - d_to_8) / 100)
 
                 p.setWeight(2-tmp)
 
