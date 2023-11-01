a96023552c038ccd9178964fd0283a6653feb7ab
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-11-01 12:41:13 +0100

    Update main_self_local.py

--- main_self_local.py
+++ main_self_local.py
@@ -63,8 +63,8 @@ def main():
                 theta_turned += 5
     
     # Calculate robot position
-    distance_to_A = landmark_dists[5]  # Distance to Landmark A
-    distance_to_B = landmark_dists[6]  # Distance to Landmark B
+    distance_to_A = landmark_dists[5] + 22.5  # Distance to Landmark A
+    distance_to_B = landmark_dists[6] + 22.5  # Distance to Landmark B
     dAB = 300.0  # Distance between Landmark A and B
 
     # Calculate robot's position
