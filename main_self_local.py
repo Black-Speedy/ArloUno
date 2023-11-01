b4d0a62ea323ad179b7bcb18ac4c6f1acaa4b1a1
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-11-01 14:01:35 +0100

    Update main_self_local.py

--- main_self_local.py
+++ main_self_local.py
@@ -78,8 +78,9 @@ def main():
     dAB = 200.0  # Distance between Landmark A and B
 
     # Calculate robot's position
+    cos_theta = (distance_to_A**2 - distance_to_B **2 + dAB**2) / (2 * dAB * distance_to_A)
     print(f"before cos theta: {cos_theta}")
-    cos_theta = min((distance_to_A**2 - distance_to_B **2 + dAB**2) / (2 * dAB * distance_to_A), 0.999)
+    cos_theta = min(cos_theta, 0.999)
     print(f"after cos theta: {cos_theta}")
 
     theta = math.acos(cos_theta)
