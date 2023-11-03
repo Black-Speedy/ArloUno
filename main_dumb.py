f32d646616818b89d6976118f972cfdbd6d34272
boaskl9 <32147244+boaskl9@users.noreply.github.com>
2023-11-03 12:13:59 +0100

    Create main_dumb.py

---
+++ main_dumb.py
@@ -0,0 +1,151 @@
+import math
+import robot_driving_states
+import numpy as np
+import time
+import camera
+import selflocalize
+from robot_driving_states import RobotController
+from rrt import *
+import sys
+import os
+sys.path.append(("Self-localization"))
+
+robot_x = 0
+robot_y = 0
+robot_theta = 0
+
+
+landmarkIDs = [8, 2, 50, 51]
+landmarks = {
+    8: (0.0, 0.0),  # Coordinates for L1
+    2: (0.0, 300.0),  # Cordinates for L2
+    50: (400.0, 0.0), # Cordinates for L3
+    51: (400.0, 300.0) # Cordinates for L4
+}
+
+landmark_dists = {
+    8: -1,
+    2: -1,
+    50: -1,
+    51: -1
+}
+
+
+def main():
+    cam = camera.Camera(0, 'arlo', useCaptureThread=True)
+
+    r = RobotController([], 0, 0, 0, FollowRRT=False)
+
+    # Find robot position
+    foundPos = False
+    theta_turned = 0.0
+
+    landmarks_found = []
+
+    ctime = time.perf_counter()
+
+    while (not foundPos):
+        if ctime + 0.001 < time.perf_counter():
+            r.update()
+            ctime = time.perf_counter()
+
+        if r.ds == robot_driving_states.DriveState.TURN:
+            continue
+
+        ids, dists, angles = cam.detect_aruco_objects(cam.get_next_frame())
+
+        if ids is not None:
+            for i in range(0, len(ids)):
+                if ids[i] == 51:
+                    continue
+                if ids[i] in landmarkIDs:
+                    if ids[i] not in landmarks_found:
+                        landmarks_found.append(ids[i])
+                    print(f"ids: {ids}, dists: {dists}, angles: {angles}")
+                    landmark_dists[ids[i]] = (dists[i])
+
+        if (len(landmarks_found) == 2):
+            foundPos = True
+        else:
+            if r.stopTimer < time.perf_counter():
+                # rotate slightly
+                r.turnDegree(15, "left")
+                r.ds = robot_driving_states.DriveState.TURN
+                theta_turned += 15
+
+    # Calculate robot position
+    A_id = landmarks_found[0]
+    B_id = landmarks_found[1]
+    distance_to_A = landmark_dists[A_id] + 22.5  # Distance to Landmark A
+    distance_to_B = landmark_dists[B_id] + 22.5  # Distance to Landmark B
+    # Distance between Landmark A and B
+    dAB = np.sqrt((landmarks[A_id][0] - landmarks[B_id][0])
+                  ** 2 + (landmarks[A_id][1] - landmarks[B_id][1])**2)
+    print(dAB)
+
+    # Calculate robot's position
+    cos_theta = (distance_to_A**2 - distance_to_B **
+                 2 + dAB**2) / (2 * dAB * distance_to_A)
+    print(f"before cos theta: {cos_theta}")
+
+    theta = math.acos(cos_theta)
+
+    y = distance_to_A * math.cos(theta)
+    x = distance_to_A * math.sin(theta)
+
+    if landmarks_found[1] == landmarkIDs[1]:  # We found L1 then L2
+        if theta_turned < 180:
+            x = -x
+
+    elif landmarks_found[1] == landmarkIDs[2]:  # We found L1 then L3
+        if theta_turned > 180:
+            tmp = x
+            x = y
+            y = -tmp
+
+    print(f"theta turned: {theta_turned}")
+    print(
+        f"distance to a: {distance_to_A}, distance to b: {distance_to_B}, theta: {theta}\n robot pose x: {x}, y: {y}")
+
+    # theta need to be adjusted, as we use the angle from the first point when we see the landmark.
+    pos = (x, y, np.arctan2(
+        (landmarks[landmarks_found[1]][1] - y), (landmarks[landmarks_found[1]][0] - x)))
+    print(f"robots theta: {pos[2]}, in deg {np.rad2deg(pos[2])}")
+
+    # turn back theta turned:
+    r.turnDegree(theta_turned, "right")
+
+    # Take a picture and look for L1
+    ids, dists, angles = cam.detect_aruco_objects(cam.get_next_frame())
+
+    if ids is not None:
+        for i in range(0, len(ids)):
+            if ids[i] == landmarkIDs[0]:
+                # First rotate towards it
+                if angles[i] > 0:
+                    r.turnDegree(np.rad2deg(-angles[i]), "right")
+                else:
+                    r.turnDegree(np.rad2deg(angles[i]), "left")
+
+            while (r.ds == robot_driving_states.DriveState.TURN):
+                if ctime + 0.001 < time.perf_counter():
+                    r.update()
+                    ctime = time.perf_counter()
+
+            # Drive towards it
+            r.straight64(dists[i] - 25)
+            r.ds = robot_driving_states.DriveState.STRAIGHT
+
+            while (r.ds == robot_driving_states.DriveState.STRAIGHT):
+                if ctime + 0.001 < time.perf_counter():
+                    r.update()
+                    ctime = time.perf_counter()
+    else:
+        print("Could not find L1")
+        return
+
+
+
+
+if __name__ == '__main__':
+    main()
