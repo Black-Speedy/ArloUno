32f1aba124d2d50020c61bca68ea8793e217d500
Mikkelbos <mikkeldollerup@gmail.com>
2023-11-03 13:23:17 +0100

    Update main_dumb.py

--- main_dumb.py
+++ main_dumb.py
@@ -92,14 +92,13 @@ def main():
 
     y = distance_to_A * math.cos(theta)
     x = distance_to_A * math.sin(theta)
-    pos_vec = np.abs(np.array([x, y]))
+    pos_toL2vec = np.abs(np.array([x-r.x, landmarks[landmarks_found[0]][1]-y]))
+    robot_pos = np.array([r.x, r.y])
     print(f"position vector: {pos_vec}")
-    L1vec = np.array([landmarks[landmarks_found[0]][0], landmarks[landmarks_found[0]][1]])
+    L1L2vec = np.array([landmarks[landmarks_found[0]][0], landmarks[landmarks_found[0]][1]])
     
     #Calculate angle between L1 and position vector
-    nom = np.rad2deg(np.arccos(np.dot(pos_vec, L1vec)))
-    de_nom = (np.linalg.norm(pos_vec) * np.linalg.norm(L1vec))
-    angle = nom/denom
+    angle = np.rad2deg(np.arccos(np.dot(pos_vec, L1L2vec) / (np.linalg.norm(pos_vec) * np.linalg.norm(L1L2vec))))
     print(f"Pos to L1 angle: {angle}")
     
 
